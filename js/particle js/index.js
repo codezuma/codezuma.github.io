b0663e64d27988c2ad1de9372699c18cd30ea839
chandresh Patidar <patidarchandresh2002@gmail.com>
2021-06-28 09:19:47 +0530

    removed particle bg for small devices and added header shadow

--- js/particle js/index.js
+++ js/particle js/index.js
@@ -1,3 +1,5 @@
+/* function for toggle switch in menu bar for small devices */
+
 document.getElementById('menu_icon').onclick = function () {
     this.classList.toggle('opened');
     this.setAttribute('aria-expanded', this.classList.contains('opened'));
@@ -6,3 +8,17 @@ document.getElementById('menu_icon').onclick = function () {
     side_menu_bar.classList.toggle('aside_open');
     side_menu_bar.classList.toggle('aside_close');
 }
+
+//animation onscroll for elements
+window.onscroll = function (e) {
+        var header = document.getElementsByTagName('header')[0];
+        if (window.scrollY !== 0 ){
+             header.style.background = " var(--secondary_bg)";
+             header.style.boxShadow = ' 0px 8px 18px -6px rgba(82, 82, 83, 0.12), 0px 12px 42px -4px rgba(59, 60, 61, 0.12)';
+            }
+        else{
+            header.style.background = "inherit";
+            header.style.boxShadow = ' none';
+
+        }
+   }
